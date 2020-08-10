a9a33cad760dd36ec11edc14bc8696fe41b7e13e
chaunceysun <chauncey_suncx@163.com>
2020-08-11 00:00:03 +0800

    内置对象第二课

--- src/httpreader/MyServer.java
+++ src/httpreader/MyServer.java
@@ -7,23 +7,26 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * @author Chauncey 2020/8/10 23:35
+ */
 public class MyServer {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
-            //模拟一个我自己的服务  读取一次浏览器发送的请求
+            System.out.println("服务器开启");
             //开启一个服务
-            System.out.println("我自己的服务器开启啦");
             ServerSocket server = new ServerSocket(9999);
             //等待客户端连接
             Socket socket = server.accept();
-            //通过socket对象读取浏览器发送的请求
-            InputStream is = socket.getInputStream();//通过socket获取一个字节输入流
-            InputStreamReader isr = new InputStreamReader(is);//字节转化成字符的流
-            BufferedReader br = new BufferedReader(isr);//只是为了要里面的readLine方法
-            //读取请求协议头信息
+            //通过socket对象读取浏览器发送的请求，获取一个字节输入流
+            InputStream is = socket.getInputStream();
+            //字节流转化成字符流
+            InputStreamReader isr = new InputStreamReader(is);
+            //因为是按行读取的，为了里面的readLine方法
+            BufferedReader br = new BufferedReader(isr);
+            //读取请求头协议信息
             String value = br.readLine();
-            while(value!=null && ""!=value){
+            while (value != null && !"".equals(value)) {
                 System.out.println(value);
                 value = br.readLine();
             }
